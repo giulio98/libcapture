e5341f31cc9d7229f70381b14c03f61fe2b96c53
lulor <lorenzo.lu@mailbox.org>
2021-08-12 12:57:04 +0200

    Cleanup

--- screen-recorder-ffmpeg-cpp-master/src/ScreenRecorder.cpp
+++ screen-recorder-ffmpeg-cpp-master/src/ScreenRecorder.cpp
@@ -34,8 +34,8 @@ int ScreenRecorder::CaptureVideoFrames() {
     int flag;
     int frameFinished;  // when you decode a single packet, you still don't have information enough to have a frame
                         // [depending on the type of codec, some of them //you do], when you decode a GROUP of packets
-                        // that represents a frame, then you have a picture! that's why frameFinished will let //you know
-                        // you decoded enough to have a frame.
+                        // that represents a frame, then you have a picture! that's why frameFinished will let //you
+                        // know you decoded enough to have a frame.
 
     int frame_index = 0;
     value = 0;
@@ -302,49 +302,13 @@ int ScreenRecorder::init_outputfile() {
     outAVFormatContext = NULL;
     value = 0;
     output_file = "../media/output.mp4";
-    // options=NULL;
 
+    /* allocate outAVFormatContext */
     avformat_alloc_output_context2(&outAVFormatContext, NULL, NULL, output_file);
     if (!outAVFormatContext) {
         cout << "\nerror in allocating av format output context";
         exit(1);
     }
-
-    /* Returns the output format in the list of registered output formats which best matches the provided parameters, or
-     * returns NULL if there is no match. */
-    /*output_format = const_cast<AVOutputFormat*>(av_guess_format(NULL, output_file ,NULL));
-    if( !output_format )
-    {
-     cout<<"\nerror in guessing the video format. try with correct format";
-     exit(1);
-    }*/
-
-    // int number_of_streams = pAVFormatContext->nb_streams;
-    // int *streams_list = (int*)av_mallocz_array(number_of_streams, sizeof(*streams_list));
-    // if(!streams_list) {
-    // 	cout << "Error in allocating streams_list";
-    // 	exit(1);
-    // }
-
-    // for (int i = 0; i < pAVFormatContext->nb_streams; i++) {
-    // 	AVStream *out_stream;
-    // 	AVStream *in_stream = pAVFormatContext->streams[i];
-    // 	AVCodecParameters *in_codecpar = in_stream->codecpar;
-    // 	if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
-    // 	    in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
-    // 	    in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
-    // 	  streams_list[i] = -1;
-    // 	  continue;
-    // }
-    // }
-
-    // value = avcodec_parameters_copy(video_st->codecpar, pAVFormatContext->streams[VideoStreamIndx]->codecpar);
-    // if (value < 0) {
-    // 	cout << "\nerror copying parameters";
-    // 	exit(1);
-    // }
-
-    av_dump_format(outAVFormatContext, 0, output_file, 1);
 
     /* create empty video file */
     if (!(outAVFormatContext->flags & AVFMT_NOFILE)) {
@@ -353,39 +317,28 @@ int ScreenRecorder::init_outputfile() {
             exit(1);
         }
     }
+
     video_st = avformat_new_stream(outAVFormatContext, NULL);
     if (!video_st) {
         cout << "\nerror in creating a av format new stream";
         exit(1);
     }
-    video_st->codecpar->codec_id =
-        AV_CODEC_ID_MPEG4;  // AV_CODEC_ID_MPEG4; // AV_CODEC_ID_H264 // AV_CODEC_ID_MPEG1VIDEO
+
+    video_st->codecpar->codec_id = AV_CODEC_ID_MPEG4;
     video_st->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
-    // video_st->codecpar->pix_fmt  = AV_PIX_FMT_YUV420P;
     video_st->codecpar->bit_rate = 400000;  // 2500000
     video_st->codecpar->width = 1920;
     video_st->codecpar->height = 1080;
-    // video_st->codecpar->gop_size = 3;
-    // video_st->codecpar->max_b_frames = 2;
-    // video_st->codecpar->time_base.num = 1;
-    // video_st->codecpar->time_base.den = 30; // 15fps
 
-    /* TO-DO: remove */
-    // outAVCodecContext = avcodec_alloc_context3(outAVCodec);
-    // if( !outAVCodecContext )
-    // {
-    //   	cout<<"\nerror in allocating the codec contexts";
-    // 	exit(1);
-    // }
     outAVCodec = const_cast<AVCodec *>(avcodec_find_encoder(AV_CODEC_ID_MPEG4));
     if (!outAVCodec) {
         cout << "\nerror in finding the av codecs. try again with correct codec";
         exit(1);
     }
+
     /* set property of the video file */
     outAVCodecContext = avcodec_alloc_context3(outAVCodec);
-    outAVCodecContext->codec_id =
-        AV_CODEC_ID_MPEG4;  // AV_CODEC_ID_MPEG4; // AV_CODEC_ID_H264 // AV_CODEC_ID_MPEG1VIDEO
+    outAVCodecContext->codec_id = AV_CODEC_ID_MPEG4;  // AV_CODEC_ID_H264 // AV_CODEC_ID_MPEG1VIDEO
     outAVCodecContext->codec_type = AVMEDIA_TYPE_VIDEO;
     outAVCodecContext->pix_fmt = AV_PIX_FMT_YUV420P;
     outAVCodecContext->bit_rate = 400000;  // 2500000
@@ -400,9 +353,10 @@ int ScreenRecorder::init_outputfile() {
         av_opt_set(outAVCodecContext->priv_data, "preset", "slow", 0);
     }
 
-    /* Some container formats (like MP4) require global headers to be present
-       Mark the encoder so that it behaves accordingly. */
-
+    /*
+     * Some container formats (like MP4) require global headers to be present
+     * Mark the encoder so that it behaves accordingly.
+     */
     if (outAVFormatContext->oformat->flags & AVFMT_GLOBALHEADER) {
         outAVCodecContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
     }
@@ -418,18 +372,16 @@ int ScreenRecorder::init_outputfile() {
         exit(1);
     }
 
-    /* imp: mp4 container or some advanced container file required header information*/
+    /* imp: mp4 container or some advanced container file required header information */
     value = avformat_write_header(outAVFormatContext, &options);
     if (value < 0) {
         cout << "\nerror in writing the header context";
         exit(1);
     }
 
-    /*
-    // uncomment here to view the complete video file informations
-    cout<<"\n\nOutput file information :\n\n";
-    av_dump_format(outAVFormatContext , 0 ,output_file ,1);
-    */
+    /* uncomment here to view the complete video file informations */
+    // cout<<"\n\nOutput file information :\n\n";
+    // av_dump_format(outAVFormatContext , 0 ,output_file ,1);
 
     return 0;
 }
