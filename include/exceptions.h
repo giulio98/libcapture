03656a0ba80abb9b6a8ee2febeaa25bfcc00b81d
lulor <lorenzo.lu@mailbox.org>
2021-11-12 13:30:17 +0100

    Update exceptions

--- include/exceptions.h
+++ include/exceptions.h
@@ -2,10 +2,14 @@
 
 #include <exception>
 
-struct FullException : public std::exception {
-    const char* what() const throw() { return "Internal buffer is full, read and re-try to send"; }
+struct BufferFullException : public std::exception {
+    const char* what() const throw() { return "Internal buffer is full, read from it and re-try to send"; }
 };
 
-struct EmptyException : public std::exception {
-    const char* what() const throw() { return "Internal buffer is empty, send and re-try to read"; }
+struct BufferEmptyException : public std::exception {
+    const char* what() const throw() { return "Internal buffer is empty, send data and re-try to read"; }
+};
+
+struct BufferFlushedException : public std::exception {
+    const char* what() const throw() { return "Internal buffer has been flushed"; }
 };
