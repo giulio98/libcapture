7e7c815f6f37df4f2b6e1ad3f76e50d407159801
lulor <lorenzo.lu@mailbox.org>
2021-11-13 18:15:56 +0100

    Set audio bit-rate

--- src/audio_encoder.cpp
+++ src/audio_encoder.cpp
@@ -7,6 +7,6 @@ AudioEncoder::AudioEncoder(AVCodecID codec_id, std::map<std::string, std::string
     codec_ctx_->channel_layout = av_get_default_channel_layout(params->channels);
     codec_ctx_->sample_rate = params->sample_rate;
     codec_ctx_->sample_fmt = codec_->sample_fmts[0];  // for aac there is AV_SAMPLE_FMT_FLTP = 8
-    // codec_ctx_->bit_rate = 96000;
+    codec_ctx_->bit_rate = 96000;
     codec_ctx_->time_base = (AVRational){1, params->sample_rate};
 }
