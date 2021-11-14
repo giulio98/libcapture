aefa140cc56a6cfb7aa28e384c03b2734dfc0c43
lulor <lorenzo.lu@mailbox.org>
2021-11-14 17:40:21 +0100

    VideoConterter: rescale PTS

--- include/video_converter.h
+++ include/video_converter.h
@@ -10,9 +10,11 @@ class VideoConverter {
     AVPixelFormat out_pix_fmt_;
     SwsContext *ctx_;
     AVFrame *converted_frame_;
+    AVRational codec_ctx_time_base_;
+    AVRational stream_time_base_;
 
 public:
-    VideoConverter(const AVCodecContext *in_codec_ctx, const AVCodecContext *out_codec_ctx);
+    VideoConverter(const AVCodecContext *in_codec_ctx, const AVCodecContext *out_codec_ctx, AVRational stream_time_base);
 
     ~VideoConverter();
 
--- src/video_converter.cpp
+++ src/video_converter.cpp
@@ -1,13 +1,15 @@
 #include "../include/video_converter.h"
 
-VideoConverter::VideoConverter(const AVCodecContext *in_codec_ctx, const AVCodecContext *out_codec_ctx)
-    : ctx_(nullptr), converted_frame_(nullptr) {
+VideoConverter::VideoConverter(const AVCodecContext *in_codec_ctx, const AVCodecContext *out_codec_ctx,
+                               AVRational stream_time_base)
+    : ctx_(nullptr), converted_frame_(nullptr), stream_time_base_(stream_time_base) {
     if (!in_codec_ctx) throw std::runtime_error("VideoConverter: in_codec_ctx is NULL");
     if (!out_codec_ctx) throw std::runtime_error("VideoConverter: out_codec_ctx is NULL");
 
     out_width_ = out_codec_ctx->width;
     out_height_ = out_codec_ctx->height;
     out_pix_fmt_ = out_codec_ctx->pix_fmt;
+    codec_ctx_time_base_ = out_codec_ctx->time_base;
 
     ctx_ = sws_getContext(in_codec_ctx->width, in_codec_ctx->height, in_codec_ctx->pix_fmt, out_width_, out_height_,
                           out_pix_fmt_, SWS_BICUBIC, nullptr, nullptr, nullptr);
@@ -32,6 +34,6 @@ const AVFrame *VideoConverter::convertFrame(const AVFrame *in_frame, int frame_n
     if (sws_scale(ctx_, in_frame->data, in_frame->linesize, 0, out_height_, converted_frame_->data,
                   converted_frame_->linesize) < 0)
         throw std::runtime_error("VideoEncoder: failed to convert frame");
-    converted_frame_->pts = frame_number;
+    converted_frame_->pts = av_rescale_q(frame_number, codec_ctx_time_base_, stream_time_base_);
     return converted_frame_;
 }
