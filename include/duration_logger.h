a3a99d697ac2a932d693d1ea83087c29188e4e84
lulor <lorenzo.lu@mailbox.org>
2021-11-15 16:42:51 +0100

    Update DurationLogger to optionally print CR

--- include/duration_logger.h
+++ include/duration_logger.h
@@ -8,15 +8,16 @@
 class DurationLogger {
     std::string msg_;
     int64_t start_time_;
+    bool new_line_;
 
 public:
-    DurationLogger(const std::string &msg) {
-        msg_ = msg;
+    DurationLogger(const std::string &msg, bool new_line = true) : msg_(msg), new_line_(new_line) {
         start_time_ = av_gettime();
     }
 
     ~DurationLogger() {
         int64_t elapsed_time = (av_gettime() - start_time_) / 1000;
         std::cout << msg_ << elapsed_time << " ms";
+        if (new_line_) std::cout << std::endl;
     }
 };
