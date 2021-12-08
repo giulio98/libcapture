bdbec81c21ecac8f769c3776d6d1310bd3905dbd
lulor <lorenzo.lu@mailbox.org>
2021-12-08 14:25:36 +0100

    Improve main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "../include/screen_recorder.h"
 
-int main(int argc, char **argv) {
-    std::string output_file("output.mp4");
+std::tuple<int, std::string, bool> get_params(std::vector<std::string> args) {
     int framerate = 30;
+    std::string output_file = "output.mp4";
     bool capture_audio = true;
+
+    for (auto it = args.begin(); it != args.end(); it++) {
+        if (*it == "-f") {
+            if (++it == args.end()) throw std::runtime_error("Wrong args");
+            framerate = atoi(it->c_str());
+        } else if (*it == "-o") {
+            if (++it == args.end()) throw std::runtime_error("Wrong args");
+            output_file = *it;
+        } else if (*it == "-mute") {
+            capture_audio = false;
+        } else {
+            throw std::runtime_error("Unknown arg: " + *it);
+        }
+    }
+
+    return std::make_tuple(framerate, output_file, capture_audio);
+}
+
+int main(int argc, char **argv) {
+    int framerate;
+    std::string output_file;
+    bool capture_audio;
     std::mutex m;
     std::condition_variable cv;
     bool pause = false;
     bool resume = false;
     bool stop = false;
 
-    if (argc == 3) {
-        output_file = argv[1];
-        framerate = atoi(argv[2]);
-    } else if (argc != 1) {
-        std::cerr << "Usage: " << argv[0] << "[output_file] [framerate]" << std::endl;
+    try {
+        std::vector<std::string> args;
+        for (int i = 1; i < argc; i++) {
+            args.push_back(argv[i]);
+        }
+        std::tie(framerate, output_file, capture_audio) = get_params(args);
+    } catch (const std::exception &e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [-f framerate] [-o output_file] [-mute]" << std::endl;
         return 1;
     }
 
@@ -49,6 +77,10 @@ int main(int argc, char **argv) {
             resume = true;
         } else if (input == 's') {
             stop = true;
+        } else {
+            if (input != 10)  // ignore CR
+                std::cerr << "Unknown option, possible options are: p[ause], r[esume], s[top]" << std::endl;
+            continue;
         }
         cv.notify_one();
     }
