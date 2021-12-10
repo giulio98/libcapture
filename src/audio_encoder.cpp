8cab8d9509cb9820af4af30fb043aae0a6ceb4e0
giulio98 <79860892+giulio98@users.noreply.github.com>
2021-12-10 12:03:39 +0100

    no use AVRATIONAL

--- src/audio_encoder.cpp
+++ src/audio_encoder.cpp
@@ -8,7 +8,8 @@ AudioEncoder::AudioEncoder(AVCodecID codec_id, const std::map<std::string, std::
     codec_ctx_->sample_rate = params->sample_rate;
     codec_ctx_->sample_fmt = codec_->sample_fmts[0];  // for aac there is AV_SAMPLE_FMT_FLTP = 8
     // codec_ctx_->bit_rate = 96000;
-    codec_ctx_->time_base = (AVRational){1, params->sample_rate};
+    codec_ctx_->time_base.num = 1;
+    codec_ctx_->time_base.den = params->sample_rate;
 
     if (global_header_flags & AVFMT_GLOBALHEADER) codec_ctx_->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
--- src/video_encoder.cpp
+++ src/video_encoder.cpp
@@ -7,8 +7,10 @@ VideoEncoder::VideoEncoder(AVCodecID codec_id, const std::map<std::string, std::
     codec_ctx_->width = params->width;
     codec_ctx_->height = params->height;
     codec_ctx_->pix_fmt = pix_fmt;
-    codec_ctx_->framerate = (AVRational){framerate, 1};
-    codec_ctx_->time_base = (AVRational){1, framerate};
+    codec_ctx_->framerate.num = framerate;
+    codec_ctx_->framerate.den = 1;
+    codec_ctx_->time_base.num = 1;
+    codec_ctx_->time_base.den = framerate;
 
     if (global_header_flags & AVFMT_GLOBALHEADER) codec_ctx_->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
