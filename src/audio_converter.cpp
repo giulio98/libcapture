8687bd56669bd976598a2d649e20b4891fe6a46c
lulor <lorenzo.lu@mailbox.org>
2021-12-16 19:23:00 +0100

    Fix memory leak

--- src/audio_converter.cpp
+++ src/audio_converter.cpp
@@ -42,10 +42,16 @@ bool AudioConverter::sendFrame(const AVFrame *frame) const {
         if (av_audio_fifo_write(fifo_buf_.get(), (void **)buf, frame->nb_samples) < 0)
             throw std::runtime_error("AudioConverter: failed to write to fifo");
 
-        if (**buf) av_freep(&buf[0]);
+        if (buf) {
+            av_freep(&buf[0]);
+            free(buf);
+        }
 
     } catch (const std::exception &e) {
-        if (**buf) av_freep(&buf[0]);
+        if (buf) {
+            av_freep(&buf[0]);
+            free(buf);
+        }
         throw;
     }
 
