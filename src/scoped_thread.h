abc2eba1c20f35738713106f61dbfebbdf7b79fa
lulor <lorenzo.lu@mailbox.org>
2021-12-23 23:46:18 +0100

    Update ScopedThread class

--- src/scoped_thread.h
+++ src/scoped_thread.h
@@ -4,21 +4,20 @@ class ScopedThread {
     std::thread t_;
 
 public:
-    ScopedThread() = default;
+    ScopedThread() noexcept = default;
 
-    ScopedThread(std::thread &&t) : t_(std::move(t)) {}
+    ScopedThread(std::thread &&t) noexcept : t_(std::move(t)) {}
 
     ScopedThread(const ScopedThread &) = delete;
 
-    ScopedThread(ScopedThread &&src) : t_(std::move(src.t_)) {}
+    ScopedThread(ScopedThread &&other) noexcept : t_(std::move(other.t_)) {}
 
     ~ScopedThread() {
         if (t_.joinable()) t_.join();
     }
 
-    ScopedThread &operator=(ScopedThread &&src) {
-        if (t_.joinable()) throw std::logic_error("Active thread already present");
-        t_ = std::move(src.t_);
+    ScopedThread &operator=(ScopedThread &&other) noexcept {
+        t_ = std::move(other.t_);
         return *this;
     }
 
