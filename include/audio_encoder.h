a06f8aec48b2fdf81b1684e33d7af4b56081cfa4
lulor <lorenzo.lu@mailbox.org>
2021-12-29 17:21:11 +0100

    AudioEncoder refactoring

--- include/audio_encoder.h
+++ include/audio_encoder.h
@@ -5,6 +5,6 @@
 
 class AudioEncoder : public Encoder {
 public:
-    AudioEncoder(AVCodecID codec_id, const std::map<std::string, std::string> &options, int global_header_flags,
-                 int channels, int sample_rate);
+    AudioEncoder(AVCodecID codec_id, const std::map<std::string, std::string> &options, const AVCodecContext *dec_ctx,
+                 int global_header_flags);
 };
--- src/audio_encoder.cpp
+++ src/audio_encoder.cpp
@@ -1,15 +1,14 @@
 #include "audio_encoder.h"
 
 AudioEncoder::AudioEncoder(AVCodecID codec_id, const std::map<std::string, std::string> &options,
-                           int global_header_flags, int channels, int sample_rate)
+                           const AVCodecContext *dec_ctx, int global_header_flags)
     : Encoder(codec_id) {
-    codec_ctx_->channels = channels;
-    codec_ctx_->channel_layout = av_get_default_channel_layout(channels);
-    codec_ctx_->sample_rate = sample_rate;
+    codec_ctx_->sample_rate = dec_ctx->sample_rate;
+    codec_ctx_->channel_layout = dec_ctx->channel_layout;
+    codec_ctx_->channels = av_get_channel_layout_nb_channels(codec_ctx_->channel_layout);
     codec_ctx_->sample_fmt = codec_->sample_fmts[0];  // for aac there is AV_SAMPLE_FMT_FLTP = 8
-    // codec_ctx_->bit_rate = 96000;
     codec_ctx_->time_base.num = 1;
-    codec_ctx_->time_base.den = sample_rate;
+    codec_ctx_->time_base.den = codec_ctx_->sample_rate;
 
     if (global_header_flags & AVFMT_GLOBALHEADER) codec_ctx_->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
