734c5a861bee30269604d470618934b0df56167c
lulor <lorenzo.lu@mailbox.org>
2022-01-01 15:35:29 +0100

    AudioEncoder: keep che channel layout from the decoder if it's known

--- src/audio_encoder.cpp
+++ src/audio_encoder.cpp
@@ -3,17 +3,21 @@
 AudioEncoder::AudioEncoder(AVCodecID codec_id, const std::map<std::string, std::string> &options,
                            const AVCodecContext *dec_ctx, int global_header_flags)
     : Encoder(codec_id) {
-    AVCodecContext *codec_ctx = getCodecContextMod();
+    AVCodecContext *enc_ctx = getCodecContextMod();
 
-    codec_ctx->sample_rate = dec_ctx->sample_rate;
-    /* Take the channels from the decoder (NOT the channel layout, since it may not be set e.g. on Linux) */
-    codec_ctx->channels = dec_ctx->channels;
-    codec_ctx->channel_layout = av_get_default_channel_layout(codec_ctx->channels);
-    codec_ctx->sample_fmt = getCodec()->sample_fmts[0];  // for aac there is AV_SAMPLE_FMT_FLTP = 8
-    codec_ctx->time_base.num = 1;
-    codec_ctx->time_base.den = codec_ctx->sample_rate;
+    enc_ctx->sample_rate = dec_ctx->sample_rate;
+    if (dec_ctx->channel_layout) { /* if a specific channel layout is known to the decoder, keep it */
+        enc_ctx->channel_layout = dec_ctx->channel_layout;
+        enc_ctx->channels = av_get_channel_layout_nb_channels(enc_ctx->channel_layout);
+    } else { /* otherwise, use the default channel layout */
+        enc_ctx->channels = dec_ctx->channels;
+        enc_ctx->channel_layout = av_get_default_channel_layout(enc_ctx->channels);
+    }
+    enc_ctx->sample_fmt = getCodec()->sample_fmts[0];
+    enc_ctx->time_base.num = 1;
+    enc_ctx->time_base.den = enc_ctx->sample_rate;
 
-    if (global_header_flags & AVFMT_GLOBALHEADER) codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    if (global_header_flags & AVFMT_GLOBALHEADER) enc_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
     init(options);
 }
