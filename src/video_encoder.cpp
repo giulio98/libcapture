c19a14a5ec97b01333bddb79801a0571ceab7452
lulor <lorenzo.lu@mailbox.org>
2022-01-01 15:34:53 +0100

    VideoEncoder: rename local variable

--- src/video_encoder.cpp
+++ src/video_encoder.cpp
@@ -3,14 +3,14 @@
 VideoEncoder::VideoEncoder(AVCodecID codec_id, const std::map<std::string, std::string> &options, int width, int height,
                            AVPixelFormat pix_fmt, AVRational time_base, int global_header_flags)
     : Encoder(codec_id) {
-    AVCodecContext *codec_ctx = getCodecContextMod();
+    AVCodecContext *enc_ctx = getCodecContextMod();
 
-    codec_ctx->width = width;
-    codec_ctx->height = height;
-    codec_ctx->pix_fmt = pix_fmt;
-    codec_ctx->time_base = time_base;
+    enc_ctx->width = width;
+    enc_ctx->height = height;
+    enc_ctx->pix_fmt = pix_fmt;
+    enc_ctx->time_base = time_base;
 
-    if (global_header_flags & AVFMT_GLOBALHEADER) codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    if (global_header_flags & AVFMT_GLOBALHEADER) enc_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
     init(options);
 }
