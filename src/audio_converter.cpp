60bc3220a4763b6bc2d1e3cabd715583457607b4
lulor <lorenzo.lu@mailbox.org>
2022-01-03 22:28:59 +0100

    Use the "aresample" audio filter instead of "aformat"

--- src/audio_converter.cpp
+++ src/audio_converter.cpp
@@ -18,13 +18,21 @@ AudioConverter::AudioConverter(const AVCodecContext *dec_ctx, const AVCodecConte
     }
 
     std::stringstream filter_spec_ss;
-    /* number of samples for output frames */
-    filter_spec_ss << "asetnsamples=n=" << enc_ctx->frame_size;
-    /* format conversion */
-    filter_spec_ss << ",aformat=sample_fmts=" << av_get_sample_fmt_name(enc_ctx->sample_fmt)
-                   << ":sample_rates=" << enc_ctx->sample_rate << ":channel_layouts=" << enc_ctx->channel_layout;
-    /* PTS */
-    filter_spec_ss << ",asetpts=PTS-STARTPTS";
+    /* set PTS */
+    filter_spec_ss << "asetpts=PTS-STARTPTS";
+    /*
+     * convert sample_rate, sample_fmt and channel layout
+     * the async=1 option tells the filter to add extra silence / stretch / squeeze the audio
+     * to ensure that the timestamps are correct
+     */
+    filter_spec_ss << ",aresample=" << enc_ctx->sample_rate << ":async=1"
+                   << ":out_sample_fmt=" << enc_ctx->sample_fmt << ":out_channel_layout=" << enc_ctx->channel_layout;
+    /* ensure correct number of samples for output frames (even with injected silence) */
+    filter_spec_ss << ",asetnsamples=n=" << enc_ctx->frame_size;
+
+    /* format conversion (OLD, now use aresample instead) */
+    // filter_spec_ss << ",aformat=sample_fmts=" << av_get_sample_fmt_name(enc_ctx->sample_fmt)
+    //                << ":sample_rates=" << enc_ctx->sample_rate << ":channel_layouts=" << enc_ctx->channel_layout;
 
     init("abuffer", "abuffersink", src_args_ss.str(), filter_spec_ss.str());
 }
